728f499db2cb2db62213313c34cbf05234a39889
Israel Bergenstein <israel.bergenstein@gmail.com>
2019-09-06 13:29:49 +0100

    Initial Commit

---
+++ ToDoTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoTry
+//
+//  Created by Israel Bergenstein on 06/09/2019.
+//  Copyright © 2019 Israel Bergenstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
